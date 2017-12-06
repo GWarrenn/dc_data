028863fbab9818b962c11eaa1516a441076d35b9
GWarrenn <augustjwarren@gmail.com>
2017-12-06 14:38:44 -0500

    12/6/2017: Adding analysis

--- scripts/dc_housing_metro.R
+++ scripts/dc_housing_metro.R
@@ -19,8 +19,7 @@ housing <- read.csv("data/real_estate/September2017Sales.csv", stringsAsFactors 
 
 housing$Address <- gsub("#.*$","",housing$Address) 
 
-for(i in 1:nrow(housing))
-{
+for(i in 1:nrow(housing)) {
   result <- geocode(paste(housing$Address[i],"Washington DC"), output = "latlona", source = "google")
   housing$lon[i] <- as.numeric(result[1])
   housing$lat[i] <- as.numeric(result[2])
@@ -52,13 +51,22 @@ closest_metro$adj_price <- as.numeric(gsub("\\$|,","",closest_metro$Close.Price)
 closest_metro$quadrant <- gsub(" ([A-Z][A-Z])$"," || \\1",closest_metro$ADDRESS)
 closest_metro$quadrant <- gsub(".* ||","",closest_metro$quadrant)
 
+for(i in 1:nrow(closest_metro)) {
+  results = gmapsdistance(origin = paste(closest_metro$lat[i],"+",closest_metro$lon[i],sep=""),
+                          destination = paste(closest_metro$Y[i],"+",closest_metro$ï..X[i],sep=""),
+                          mode = "walking")
+  closest_metro$walking_time[i] <- as.numeric(results$Time)
+  
+}  
+
 ## Overall plot of price by distance
 
-ggplot(closest_metro,aes(x=as.numeric(closest_metro$distance),y=closest_metro$adj_price)) + geom_point()
+plot_1_scatter <- ggplot(closest_metro,aes(x=as.numeric(closest_metro$distance),y=closest_metro$adj_price)) + geom_point()
 
 ## Plot of housing price broken up by Ward
 
-ggplot(closest_metro,aes(x=as.numeric(closest_metro$distance),y=closest_metro$adj_price)) + geom_point() + facet_wrap( ~ quadrant)
+plot_2_scatter_quadrant <- ggplot(closest_metro,aes(x=as.numeric(closest_metro$distance),y=closest_metro$adj_price)) + geom_point() + 
+  facet_wrap( ~ quadrant) + scale_y_continuous(labels=comma)
 
 overall_model <- lm(closest_metro$adj_price ~ closest_metro$distance)
 
@@ -76,23 +84,12 @@ summary(overall_model_w_ward)$r.squared
 
 # Testing two methods for proximity to metro: Haverstine Distance & Walking Time (Google Maps)
 
-for(i in 1:nrow(closest_metro))
-{
-  results = gmapsdistance(origin = paste(closest_metro$lat[i],"+",closest_metro$lon[i],sep=""),
-                          destination = paste(closest_metro$Y[i],"+",closest_metro$ï..X[i],sep=""),
-                          mode = "walking")
-  closest_metro$walking_time[i] <- as.numeric(results$Time)
-
-}  
-
 walking_distance <- closest_metro[closest_metro$distance <= 1000,]
 walking_time <- closest_metro[closest_metro$walking_time <= 600,]
 
 qmplot(lon,lat,data=walking_distance,maptype = "toner-lite", color = NAME) + scale_colour_discrete(guide=FALSE)
 
-map <- get_map(location = 'Washington DC', zoom = 12)
-
-ggmap(map) + stat_summary_2d(geom = "tile",bins = 50,data=closest_metro, aes(x = lon, y = lat, z = closest_metro$walking_time), alpha=0.5) +
+plot_3a_walking_map <- ggmap(map) + stat_summary_2d(geom = "tile",bins = 50,data=closest_metro, aes(x = lon, y = lat, z = closest_metro$walking_time), alpha=0.5) +
   scale_fill_gradient(low = "yellow", high = "red", guide = guide_legend(title = "Seconds")) +xlab("") + ylab("") +
   theme(axis.title=element_blank(),
         axis.text=element_blank(),
@@ -100,22 +97,20 @@ ggmap(map) + stat_summary_2d(geom = "tile",bins = 50,data=closest_metro, aes(x =
 
 closest_metro_trim <- closest_metro[closest_metro$walking_time <= 5000,]
 
-ggmap(map) + stat_summary_2d(geom = "tile",bins = 50,data=closest_metro_trim, aes(x = lon, y = lat, z = closest_metro_trim$walking_time), alpha=0.5) +
-  scale_fill_gradient(low = "yellow", high = "red", guide = guide_legend(title = "Seconds")) +xlab("") + ylab("") +
-  theme(axis.title=element_blank(),
-        axis.text=element_blank(),
-        axis.ticks=element_blank()) + ggtitle("Walking Distance to Metro")
+closest_metro_trim$minutes <- closest_metro_trim$walking_time / 60
 
+map <- get_map(location = 'Washington DC', zoom = 12)
 
-qmplot(lon,lat,data=closest_metro,maptype = "toner-lite", color = NAME) + scale_colour_discrete(guide=FALSE) + 
-    stat_summary_2d(geom = "tile",bins = 50,data=closest_metro, aes(x = lon, y = lat, z = closest_metro$walking_time), alpha=0.5)
-
+plot_3b_walking_map <- ggmap(map) + stat_summary_2d(geom = "tile",bins = 50,data=closest_metro_trim, aes(x = lon, y = lat, z = as.numeric(closest_metro_trim$minutes)), alpha=0.5) +
+  scale_fill_gradient(low = "yellow", high = "red", guide = guide_legend(title = "Minutes")) +xlab("") + ylab("") +
+  theme(axis.title=element_blank(),axis.text=element_blank(),axis.ticks=element_blank(),plot.title = element_text(hjust = 0.5),text = element_text(size=10)) + 
+  ggtitle("Walking Time to Metro")
 
 avg_price_by_distance <- walking_distance %>%
                       group_by(NAME) %>%
                       summarise(average_price=mean(adj_price))
 
-ggplot(avg_price_by_distance, aes(x = reorder(avg_price_by_distance$NAME, avg_price_by_distance$average_price), 
+plot_4a_price_dist <- ggplot(avg_price_by_distance, aes(x = reorder(avg_price_by_distance$NAME, avg_price_by_distance$average_price), 
                                y = as.numeric(avg_price_by_distance$average_price),fill=avg_price_by_distance$average_price)) + 
   geom_bar(stat = "identity",show.legend=FALSE) + coord_flip()  +
   ggtitle("Average Housing Price (Per Bedroom) within 1,000 Feet by Metro Stop") + 
@@ -127,7 +122,7 @@ avg_price_by_time <- walking_time %>%
   group_by(NAME) %>%
   summarise(average_price=mean(adj_price))
 
-ggplot(avg_price_by_time, aes(x = reorder(avg_price_by_time$NAME, avg_price_by_time$average_price), 
+plot_4b_price_time <- ggplot(avg_price_by_time, aes(x = reorder(avg_price_by_time$NAME, avg_price_by_time$average_price), 
                                   y = as.numeric(avg_price_by_time$average_price),fill=avg_price_by_time$average_price)) + 
   geom_bar(stat = "identity",show.legend=FALSE) + coord_flip()  +
   ggtitle("Average Housing Price (Per Bedroom) within 10 Minute Walk to Metro Stop") + 
@@ -140,6 +135,6 @@ ggplot(avg_price_by_time, aes(x = reorder(avg_price_by_time$NAME, avg_price_by_t
 
 ##other analysis
   #break plot out by quadrant/ward DONE
-  #average home price by metro stop
-    #--most/least expensive metro to live within 100 feet of?
+  #average home price by metro stop DONE
+    #--most/least expensive metro to live near? DONE
   #number of lines?

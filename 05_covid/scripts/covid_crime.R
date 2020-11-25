4bbbf7339fd7f1b9dba4072b24fb76af8dc40a7f
August Warren <augustwarren@Augusts-MacBook-Pro.local>
2020-11-25 11:31:47 -0500

    COVID & Crime: Updating data pull through API & Breaking out violent crimes

--- 05_covid/scripts/covid_crime.R
+++ 05_covid/scripts/covid_crime.R
@@ -6,24 +6,43 @@ library(grid)
 library(scales)
 library(mapdeck)
 
-crime_df <- read.csv("data/dc-crimes-search-results_20200623.csv")
+api_data <- GET("https://opendata.arcgis.com/datasets/f516e0dd7b614b088ad781b0c4002331_2.geojson")
 
-crime_df$month <- as.numeric(format(as.Date(crime_df$REPORT_DAT,"%Y-%m-%d"), "%m"))
-crime_df$Year <- format(as.Date(crime_df$REPORT_DAT,"%Y-%m-%d"), "%Y")
+crime_2020 <- fromJSON(rawToChar(api_data$content))
+crime_2020 <- crime_2020$features$properties
+
+api_data <- GET("https://opendata.arcgis.com/datasets/f08294e5286141c293e9202fcd3e8b57_1.geojson")
+
+crime_2019 <- fromJSON(rawToChar(api_data$content))
+crime_2019 <- crime_2019$features$properties
+
+crime_df <- rbind(crime_2020,crime_2019)
+
+crime_df$month <- as.numeric(format(as.Date(crime_df$REPORT_DAT,"%Y/%m/%d"), "%m"))
+crime_df$Year <- format(as.Date(crime_df$REPORT_DAT,"%Y/%m/%d"), "%Y")
+crime_df$day_numeric <- format(as.Date(crime_df$REPORT_DAT,"%Y/%m/%d"), "%d")
 crime_df$year_month <- as.Date(paste0(crime_df$month,"/","01/",as.numeric(crime_df$Year),sep=""),"%m/%d/%Y")
 
-crime_df$day <- as.Date(crime_df$REPORT_DAT,"%Y-%m-%d")
-crime_df$day_no_year <- format(as.Date(crime_df$REPORT_DAT,"%Y-%m-%d"),"%m/%d")
+crime_df$day <- as.Date(crime_df$REPORT_DAT,"%Y/%m/%d")
+crime_df$day_no_year <- format(as.Date(crime_df$REPORT_DAT,"%Y/%m/%d"),"%m/%d")
 
 crime_df <- crime_df %>%
-  filter(Year != "2017" & month < 7)
+  filter(Year != "2017" & ((month < 11) | (month == 11 & day_numeric <= 15)))
 
-crime_df$violent_crime = ifelse(crime_df$OFFENSE == "homicide" |
+crime_df$OFFENSE <- tolower(crime_df$OFFENSE)
+
+crime_df$violent_crime <- ifelse(crime_df$OFFENSE == "homicide" |
                                   crime_df$OFFENSE == "sex abuse" | 
                                   crime_df$OFFENSE == "assault w/dangerous weapon" |
                                   crime_df$OFFENSE == "robbery",1,0)
 
-crime_df$nonviolent_crime = ifelse(crime_df$violent_crime == 0,1,0)
+crime_df$nonviolent_crime <- ifelse(crime_df$violent_crime == 0,1,0)
+
+crime_df$homicide_assault <- ifelse(crime_df$OFFENSE == "homicide" |
+                                      crime_df$OFFENSE == "assault w/dangerous weapon",1,0)
+
+crime_df$robbery_sa <- ifelse(crime_df$OFFENSE == "robbery" |
+                                      crime_df$OFFENSE == "sex abuse",1,0)
 
 #############################################################
 ##
@@ -35,13 +54,17 @@ crime_by_day <- crime_df %>%
   group_by(Year,day) %>%
   arrange(day) %>%
   summarise(num_violent_crimes = sum(violent_crime),
-            num_nonviol_crimes = sum(nonviolent_crime)) %>%
+            num_nonviol_crimes = sum(nonviolent_crime),
+            num_homicide_assault = sum(homicide_assault),
+            num_robbery_sa = sum(robbery_sa)) %>%
   mutate(violent_crimes = cumsum(num_violent_crimes),
          nonviolent_crimes = cumsum(num_nonviol_crimes),
+         homicide_assault_cum = cumsum(num_homicide_assault),
+         robbery_sa_cum = cumsum(num_robbery_sa),
          moving_avg_violent = rollapply(num_violent_crimes,14,mean,align='right',fill=NA),
-         moving_avg_nonviolent = rollapply(num_nonviol_crimes,14,mean,align='right',fill=NA))
-
-crime_by_day <- crime_by_day %>%
+         moving_avg_nonviolent = rollapply(num_nonviol_crimes,14,mean,align='right',fill=NA),
+         moving_avg_homicide_assault = rollapply(num_homicide_assault,14,mean,align='right',fill=NA),
+         moving_avg_robbery_sa = rollapply(num_robbery_sa,14,mean,align='right',fill=NA)) %>%
   filter(Year != "2018")
 
 p2_violent <- ggplot(crime_by_day,aes(x=as.Date(paste(2020,strftime(day,format="%m-%d"),sep="-")),
@@ -50,13 +73,51 @@ p2_violent <- ggplot(crime_by_day,aes(x=as.Date(paste(2020,strftime(day,format="
                         group=Year)) + 
   geom_line(size=1) +
   geom_vline(xintercept = as.Date("2020-04-01","%Y-%m-%d")) +
-  geom_vline(xintercept = as.Date("2020-05-25","%Y-%m-%d"),linetype='dashed') +
+  geom_vline(xintercept = as.Date("2020-05-29","%Y-%m-%d"),linetype='dashed') +
   labs(x="Date",
        y="Average Number of Violent Crimes",
        title="Violent Crimes",
        color="Year") +
   theme_bw() +
-  theme(legend.position = "bottom") 
+  theme(legend.position = "bottom") +
+  scale_x_date(date_breaks = "2 months",
+               date_minor_breaks = "1 month",
+               date_labels = "%b")
+
+## WARD 1
+
+wards <- crime_df %>%
+  group_by(WARD,Year,day) %>%
+  arrange(day) %>%
+  summarise(num_violent_crimes = sum(violent_crime),
+            num_nonviol_crimes = sum(nonviolent_crime)) %>%
+  mutate(violent_crimes = cumsum(num_violent_crimes),
+         nonviolent_crimes = cumsum(num_nonviol_crimes),
+         moving_avg_violent = rollapply(num_violent_crimes,14,mean,align='right',fill=NA),
+         moving_avg_nonviolent = rollapply(num_nonviol_crimes,14,mean,align='right',fill=NA)) %>%
+  filter(Year != "2018")
+
+violent_crime_by_ward <- ggplot(wards,aes(x=as.Date(paste(2020,strftime(day,format="%m-%d"),sep="-")),
+                        y=moving_avg_violent,
+                        color=as.character(Year),
+                        group=Year)) + 
+  geom_line(size=1) +
+  facet_wrap(~WARD) +
+  geom_vline(xintercept = as.Date("2020-04-01","%Y-%m-%d")) +
+  geom_vline(xintercept = as.Date("2020-05-29","%Y-%m-%d"),linetype='dashed') +
+  labs(x="Date",
+       y="Average Number of Violent Crimes",
+       title="Average Number of Violent & Non-Violent Crimes per 14 Days in Washington DC",
+       subtitle = "by Ward",
+       color="Year",
+       caption="Source: Washington MPD Crime Incident Data | Viz: August Warren (gwarrenn.github.io) | Stay At Home Order issued April 1st & Lifted May 29th") +
+  theme_bw() +
+  theme(legend.position = "bottom") +
+  scale_x_date(date_breaks = "2 months",
+               date_minor_breaks = "1 month",
+               date_labels = "%b")
+
+ggsave(plot = violent_crime_by_ward, "images/violent_crime_by_ward_20201115.png", w = 12, h = 6)
 
 p2_nonviolent <- ggplot(crime_by_day,aes(x=as.Date(paste(2020,strftime(day,format="%m-%d"),sep="-")),
                               y=moving_avg_nonviolent,
@@ -64,16 +125,20 @@ p2_nonviolent <- ggplot(crime_by_day,aes(x=as.Date(paste(2020,strftime(day,forma
                               group=Year)) + 
   geom_line(size=1) +
   geom_vline(xintercept = as.Date("2020-04-01","%Y-%m-%d")) +
-  geom_vline(xintercept = as.Date("2020-05-25","%Y-%m-%d"),linetype='dashed') +
+  geom_vline(xintercept = as.Date("2020-05-29","%Y-%m-%d"),linetype='dashed') +
   labs(x="Date",
        y="Average Number of Non-violent Crimes",
        title="Non-violent Crimes",
        color="Year") +
   theme_bw() +
-  theme(legend.position = "bottom") 
+  theme(legend.position = "bottom") +
+  scale_x_date(date_breaks = "2 months",
+               date_minor_breaks = "1 month",
+               date_labels = "%b")
+
 
 change_plots <- grid.arrange(p2_violent,p2_nonviolent,ncol=2,
-                             bottom = textGrob("Source: Washington MPD Crime Incident Data | Viz: August Warren (gwarrenn.github.io) | Stay At Home Order issued April 1st",
+                             bottom = textGrob("Source: Washington MPD Crime Incident Data | Viz: August Warren (gwarrenn.github.io) | Stay At Home Order issued April 1st & Lifted May 29th",
                                                x = 0,
                                                y = 0.5,
                                                just = "left",
@@ -85,7 +150,64 @@ change_plots <- grid.arrange(p2_violent,p2_nonviolent,ncol=2,
                                       gp = gpar(fontsize = 18)
                              ))
 
-ggsave(plot = change_plots, "images/covid_avg_crime_20200623.png", w = 12, h = 6,type = "cairo-png")
+ggsave(plot = change_plots, "images/covid_avg_crime_20201115.png", w = 12, h = 6)
+
+## breaking up violent crimes
+
+
+p3_homicide <- ggplot(crime_by_day,aes(x=as.Date(paste(2020,strftime(day,format="%m-%d"),sep="-")),
+                                      y=moving_avg_homicide_assault,
+                                      color=as.character(Year),
+                                      group=Year)) + 
+  geom_line(size=1) +
+  geom_vline(xintercept = as.Date("2020-04-01","%Y-%m-%d")) +
+  geom_vline(xintercept = as.Date("2020-05-29","%Y-%m-%d"),linetype='dashed') +
+  labs(x="Date",
+       y="Average Number of Homicides",
+       title="Homicide & Assualt with a Deadly Weapon",
+       color="Year") +
+  theme_bw() +
+  theme(legend.position = "bottom") +
+  scale_x_date(date_breaks = "2 months",
+               date_minor_breaks = "1 month",
+               date_labels = "%b")
+
+p4_robbery_sa <- ggplot(crime_by_day,aes(x=as.Date(paste(2020,strftime(day,format="%m-%d"),sep="-")),
+                                       y=moving_avg_robbery_sa,
+                                       color=as.character(Year),
+                                       group=Year)) + 
+  geom_line(size=1) +
+  geom_vline(xintercept = as.Date("2020-04-01","%Y-%m-%d")) +
+  geom_vline(xintercept = as.Date("2020-05-29","%Y-%m-%d"),linetype='dashed') +
+  labs(x="Date",
+       y="Average Number of Robberies and Sexual Assualt",
+       title="Robberies & Sexual Assualt",
+       color="Year") +
+  theme_bw() +
+  theme(legend.position = "bottom") +
+  scale_x_date(date_breaks = "2 months",
+               date_minor_breaks = "1 month",
+               date_labels = "%b")
+
+violent_change_plots <- grid.arrange(p3_homicide,p4_robbery_sa,ncol=2,
+                             bottom = textGrob("Source: Washington MPD Crime Incident Data | Viz: August Warren (gwarrenn.github.io) | Stay At Home Order issued April 1st & Lifted May 29th",
+                                               x = 0,
+                                               y = 0.5,
+                                               just = "left",
+                                               gp = gpar(fontsize = 8)),
+                             top = textGrob("Average Number of Violent Crimes per 14 Days in Washington DC",
+                                            x = 0.03, 
+                                            y = 0.5, 
+                                            just = "left", 
+                                            gp = gpar(fontsize = 18)
+                             ))
+
+ggsave(plot = violent_change_plots, "images/violent_change_plots.png", w = 12, h = 6)
+
+crime_df %>%
+  group_by(Year) %>%
+  summarise(total_hom = sum(homicide_assault),
+            total_rob = sum(robbery_sa))
 
 #############################################################
 ##
@@ -95,7 +217,7 @@ ggsave(plot = change_plots, "images/covid_avg_crime_20200623.png", w = 12, h = 6
 
 violent_crime_by_ward <- crime_df %>%
   mutate(stay_at_home = if_else(month<4,"Pre-Stay At Home Order",
-                                if_else(month >= 6,"Post-George Floyd Murder","Post-Stay At Home Order"))) %>%
+                                if_else(month >= 6,"Stay at Home Lifted","Stay At Home Order"))) %>%
   group_by(WARD,stay_at_home,Year,day_no_year) %>%
   arrange(day_no_year) %>%
   summarise(num_violent_crimes = sum(violent_crime),
@@ -113,7 +235,7 @@ violent_crime_by_ward <- crime_df %>%
 
 nonviolent_crime_by_ward <- crime_df %>%
   mutate(stay_at_home = if_else(month<4,"Pre-Stay At Home Order",
-                                if_else(month >= 6,"Post-George Floyd Murder","Post-Stay At Home Order"))) %>%
+                                if_else(month >= 6,"Stay at Home Lifted","Stay At Home Order"))) %>%
   group_by(WARD,stay_at_home,Year,day_no_year) %>%
   arrange(day_no_year) %>%
   summarise(num_violent_crimes = sum(violent_crime),
@@ -131,7 +253,7 @@ nonviolent_crime_by_ward <- crime_df %>%
 
 combined_crimes_by_ward <- rbind(violent_crime_by_ward,nonviolent_crime_by_ward)
 
-violent_crime_by_ward$stay_at_home <- factor(violent_crime_by_ward$stay_at_home,levels = c("Pre-Stay At Home Order","Post-Stay At Home Order","Post-George Floyd Murder"))
+violent_crime_by_ward$stay_at_home <- factor(violent_crime_by_ward$stay_at_home,levels = c("Pre-Stay At Home Order","Stay At Home Order","Stay at Home Lifted"))
 violent_crime_by_ward$ward_lab <- paste("Ward",violent_crime_by_ward$WARD)
 
 ward_violent <- ggplot(violent_crime_by_ward,aes(x=stay_at_home,y=avg_change,fill=avg_change)) +
@@ -146,10 +268,11 @@ ward_violent <- ggplot(violent_crime_by_ward,aes(x=stay_at_home,y=avg_change,fil
        y="Average Change in Crime (per 14 Days)",
        title="Violent Crime -- 2019 vs. 2020",
        fill="Average Change") +
-  theme(legend.position = "bottom") +
-  scale_x_discrete(labels = function(grouping) str_wrap(grouping, width = 10))
+  theme_bw() +
+  theme(legend.position = "bottom",axis.text.x = element_text(size=8)) +
+  scale_x_discrete(labels = function(grouping) str_wrap(grouping, width = 8))
 
-nonviolent_crime_by_ward$stay_at_home <- factor(nonviolent_crime_by_ward$stay_at_home,levels = c("Pre-Stay At Home Order","Post-Stay At Home Order","Post-George Floyd Murder"))
+nonviolent_crime_by_ward$stay_at_home <- factor(nonviolent_crime_by_ward$stay_at_home,levels = c("Pre-Stay At Home Order","Stay At Home Order","Stay at Home Lifted"))
 nonviolent_crime_by_ward$ward_lab <- paste("Ward",nonviolent_crime_by_ward$WARD)
 
 ward_nonviolent <- ggplot(nonviolent_crime_by_ward,aes(x=stay_at_home,y=avg_change,fill=avg_change)) +
@@ -164,11 +287,12 @@ ward_nonviolent <- ggplot(nonviolent_crime_by_ward,aes(x=stay_at_home,y=avg_chan
        y="Average Change in Crime (per 14 Days)",
        title="Non-Violent Crime -- 2019 vs. 2020",
        fill="Average Change") +
-  theme(legend.position = "bottom") +
-  scale_x_discrete(labels = function(grouping) str_wrap(grouping, width = 10))
+  theme_bw() +
+  theme(legend.position = "bottom",axis.text.x = element_text(size=8)) +
+  scale_x_discrete(labels = function(grouping) str_wrap(grouping, width = 8))
 
 change_ward_plots <- grid.arrange(ward_violent,ward_nonviolent,ncol=2,
-                             bottom = textGrob("Source: Washington MPD Crime Incident Data | Viz: August Warren (gwarrenn.github.io) | Stay At Home Order issued April 1st",
+                             bottom = textGrob("Source: Washington MPD Crime Incident Data | Viz: August Warren (gwarrenn.github.io) | Stay At Home Order issued April 1st & Lifted May 29th",
                                                x = 0,
                                                y = 0.5,
                                                just = "left",
@@ -180,7 +304,7 @@ change_ward_plots <- grid.arrange(ward_violent,ward_nonviolent,ncol=2,
                                             gp = gpar(fontsize = 18)
                              ))
 
-ggsave(plot = change_ward_plots, "images/change_ward_plots_20200623.png", w = 12, h = 6,type = "cairo-png")
+ggsave(plot = change_ward_plots, "images/change_ward_plots_20201115.png", w = 12, h = 6,type = "quartz")
 
 ## Type of crime 
 
